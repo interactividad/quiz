ca1e8cbda5fea0301d8f4cf8a367a519ed0f075e
interactividad <interactividad@gmail.com>
2015-07-23 17:37:56 +0200

    Primera página y favicon

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Mega-Quiz',slogan:'¡Crea tus propios juegos de preguntas!' });
 });
 
 module.exports = router;
